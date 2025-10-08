f39593e99e31be97aed8bc5162212df897571bef
Saksham <guptasaksham795@gmail.com>
2025-10-08 18:44:59 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header"
+import JobCard from "./components/JobCard"
+import NavBar from "./components/NavBar"
+import SearchBar from "./components/SearchBar"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <NavBar/>
+        <Header/>
+        {/* <SearchBar/> */}
+        {/* <JobCard/> */}
+      </div>
+    </>
+  )
+}
+
+export default App
